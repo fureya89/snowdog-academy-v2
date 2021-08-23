051d3cdf202cb4635bb2b4915a6823a6079b0df5
Ewelina Z <narcyza1989@o2.pl>
2021-08-23 21:44:25 +0200

    TASK 3: Added fetching current prices of cryptocurriencies from API CoinCap

--- src/Command/UpdatePrices.php
+++ src/Command/UpdatePrices.php
@@ -18,7 +18,42 @@ class UpdatePrices
 
     public function __invoke(OutputInterface $output)
     {
-        // TODO
-        // use $this->cryptocurrencyManager->updatePrice() method
+        try {
+
+            $url = 'https://api.coincap.io/v2/assets';
+
+            $cURL = curl_init();
+
+            curl_setopt($cURL, CURLOPT_URL, $url);
+            curl_setopt($cURL, CURLOPT_HTTPGET, true);
+            curl_setopt($cURL, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($cURL, CURLOPT_ENCODING, '');
+            curl_setopt($cURL, CURLOPT_TIMEOUT, 0);
+            curl_setopt($cURL, CURLOPT_FAILONERROR, true);
+
+            curl_setopt($cURL, CURLOPT_HTTPHEADER, array(
+                'Content-Type: application/json',
+                'Accept: application/json'
+            ));
+
+            $result = curl_exec($cURL);
+
+            if(curl_errno($cURL)){
+                throw new Exception(curl_error($cURL));
+            }
+                $response = json_decode($result);
+
+                foreach ($response->data as $currency) {
+                    $this->cryptocurrencyManager->updatePrice($currency->id, round($currency->priceUsd, 2));
+                }
+
+                $output->writeln('Updated prices');
+
+        } catch(Exception $e){
+            $output->writeln('Not updated prices. Message: '.$e->getMessage());
+        }finally {
+            curl_close($cURL);
+        }
+
     }
 }
